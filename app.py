32d13bb0b8354b312dc80e94d04361197fa4784a
Concopa <157758490+Concopa@users.noreply.github.com>
2024-05-27 15:56:53 -0600

    Small update to comments

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 """
 My OpenAI chat bot. Testing to see if it will be cheaper monthly than ChatGPT
 Practice with programming in Python
+
+Current API Price (May 27, 2024)
+Model gpt-4o
+Input
+$5.00 / 1M tokens
+
+Output
+$15.00 / 1M tokens
 """
 from openai import OpenAI
 import keys
