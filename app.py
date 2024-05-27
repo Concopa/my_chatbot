dc0103a1c4085a68d41c74e764cc963fbd0d3b00
Concopa <157758490+Concopa@users.noreply.github.com>
2024-05-27 17:03:20 -0600

    Testing out openAI pythin library

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-My OpenAI chat bot. Testing to see if it will be cheaper monthly than ChatGPT
+My OpenAI chatbot. Testing to see if it will be cheaper monthly than ChatGPT
 Practice with programming in Python
 
 Current API Price (May 27, 2024)
@@ -13,6 +13,7 @@ $15.00 / 1M tokens
 from openai import OpenAI
 import keys
 
+MODEL = "gpt-4o"
 
 """
 Secret keys for OpenAI stored in keys.py
@@ -28,4 +29,24 @@ client = OpenAI(
     api_key=keys.api_key,
 )
 
-client.models.list()
+# Printing output so I remember what is going on with sample code
+print(f"You are a helpful assistant. Help me with programming python questions!")
+print(f"What is a class in python?")
+
+# Example from https://cookbook.openai.com/examples/gpt4o/introduction_to_gpt4o
+completion = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+        # This is the system message that provides context to the model
+        {"role": "system", "content": "You are a helpful assistant. Help me with programming python questions!"},
+        # This is the user message for which the model will generate a response
+        {"role": "user", "content": "What is a class in python?"}
+    ],
+    temperature=1,
+    max_tokens=512,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+print("Assistant: " + completion.choices[0].message.content)
